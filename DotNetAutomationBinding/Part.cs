9d7bfb5d7468d2ece718afeb7e3c3c988714f316
saramsey21 <saramsey21@gmail.com>
2023-04-10 18:08:29 -0400

    Inital commit Group 9

--- DotNetAutomationBinding/Part.cs
+++ DotNetAutomationBinding/Part.cs
@@ -9,17 +9,17 @@ namespace DotNetAutomationBinding
 {
     public class Part
     {
-        //TODO to setup the PINVOKE for save
+        [DllImport("DotNetAutomationNative", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        static extern void DotNet_automationapi_Part_Save(int guid);
 
 
 
         public void Save()
         {
-            //TODO
-
+            DotNet_automationapi_Part_Save(m_guid);
         }
 
-        public Part(int guid)  
+        public Part(int guid)
         {
             m_guid = guid;
         }
