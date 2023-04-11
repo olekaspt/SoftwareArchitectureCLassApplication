58951703f61dcd69a0f8f82dc820ffedb62c854f
Shubham Sorte <luk4shubhs@gmail.com>
2023-04-10 21:59:24 -0400

    Added .net binding

--- DotNetAutomationBinding/Session.cs
+++ DotNetAutomationBinding/Session.cs
@@ -6,21 +6,23 @@ namespace DotNetAutomationBinding
     public class Session
 
     {
-
+        // Import the native function for initializing the session
         [DllImport("DotNetAutomationNative", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
         static extern void DotNet_automationapi_Session_InitializeSession();
 
+        // Import the native function for creating a part and return its guid
         [DllImport("DotNetAutomationNative", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
         static extern int DotNet_automationapi_Session_MakePart(string partPath);
 
-
+        // Constructor that initializes the session by calling the native function
         private Session()
         {
             DotNet_automationapi_Session_InitializeSession();
         }
 
-
+        // A lazy-initialized instance of the session object
         private static readonly Lazy<Session> lazy = new Lazy<Session>(() => new Session());
+        // Get the session object instance
         public static Session GetSession
         {
             get
@@ -29,13 +31,15 @@ namespace DotNetAutomationBinding
             }
         }
 
-
+        // Create a new part and return a Part object instance
         public Part MakePart(string pathName)
         {
-            Part partpart = null;
-
-
-            return partpart;
+            // Part partpart = null;
+            // Call the native function to create a part and get its guid
+            int guid = DotNet_automationapi_Session_MakePart(pathName);
+            // Return a new Part object instance with the guid
+            return new Part(guid);
+            // return partpart;
         }
 
     }
